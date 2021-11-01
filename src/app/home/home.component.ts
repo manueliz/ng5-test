b94a3cda744d4baaebfaaaf76cfde4d7a5d07417
manueliz9518 <manueliz9518@gmail.com>
2021-10-31 22:37:23 -0600

    add services #11

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Optional } from '@angular/core';
 import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
+import { DataService } from '../data.service';
+
 
 @Component({
   selector: 'app-home',
@@ -34,18 +36,24 @@ export class HomeComponent implements OnInit {
   btnText:string = 'Add an item';
   goalText:string = 'My first life goal';
   goals:string[]=[];
-  constructor() { }
+
+  constructor(private _data: DataService) { }
 
   ngOnInit(): void {
+    this._data.goal.subscribe(res => this.goals = res);
+    this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   addItem(){
     this.goals.push(this.goalText);
     this.goalText='';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
   removeItem(i:any){
     this.goals.splice(i,1);
+    this._data.changeGoal(this.goals);
   }
 
 }
